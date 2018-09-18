f9dc05e67921611a6be02b018877f9a03862a835
pabloargueta <pabloargueta27@gmail.com>
2018-09-18 14:29:02 -0500

    update

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,9 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
   validates :body, presence: true, length: {minimum: 1, maximum: 250}
-  after_create_commit { MessageBroadcastJob.perform_later(self) }
+  after_create_commit do
+    MessageBroadcastJob.perform_later(self)
+  end
 
 def timestamp
   created_at.strftime('%H:%M:%S %d %B %Y')
